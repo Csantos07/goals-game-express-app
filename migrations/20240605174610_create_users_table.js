4357ca6571707e0373b738a29010458706415353
Carlo Santos <rcarlosantos89@gmail.com>
2024-06-10 08:25:45 -0500

    created users model and users_goals table; finished basic crud for users

---
+++ migrations/20240605174610_create_users_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+    table.string('first_name').notNullable();
+    table.string('last_name').notNullable();
+    table.string('username').unique().notNullable();
+    table.string('email').unique().notNullable();
+    table.string('password').notNullable();
+    table.integer('points').defaultTo(0);
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('users');
+};
